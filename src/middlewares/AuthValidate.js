47b76eaf2e0b46f7a7e42e759d22ddd3964a095f
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-04-23 17:46:03 +0000

    fix crash server on error message

--- src/middlewares/AuthValidate.js
+++ src/middlewares/AuthValidate.js
@@ -1,20 +1,22 @@
-exports.userSignUpValidator = (req, res) => {
+exports.userSignUpValidator = (req, res, next) => {
   // Smart check
-  req.check("email", "Email is Required !").notEmpty().isEmail();
+  req.check("email", "Email is Required ").notEmpty().isEmail();
   req
     .check("password", "Password is Required !")
     .notEmpty()
     .isLength({ min: 6, max: 20 })
     .withMessage("Password must between 6 and 20 Caracters");
-  
-    // Get the foun errors
+
+  // Get the foun errors
   const errors = req.validationErrors();
-  
+
   // Render errors
   if (errors) {
-    res.status(400);
     // Send all the messages : errors
     // Send message by message : errors[0].msg
-    res.send({ statusCode: 400, success: false, statusMessage: errors[0].msg });
+    return res
+      .status(400)
+      .send({ statusCode: 400, success: false, statusMessage: errors[0].msg });
   }
+  next();
 };
