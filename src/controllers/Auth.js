942563952b090d49ca8182593966320cd90f2965
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-04-23 17:16:25 +0000

    error message

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -9,20 +9,34 @@ exports.singUp = async (_req, _res) => {
   // Extract information from the request body
   const { email, password } = _req.body;
   const user = new User({ email, password });
-  // Save it in DB
-  await user.save((err, res) => {
-    if (err) {
-      _res.status(400);
-      _res.send({ statusCode: 400, success: false, statusMessage: err });
-    } else {
-      const { email, password, _id } = res;
-      let data = {
-        email,
-        password,
-        id: _id,
-      };
-      _res.status(200);
-      _res.send({ statusCode: 200, success: true, data });
-    }
-  });
+  try {
+    // Save it in DB
+    await user.save((err, res) => {
+      if (err) {
+        _res.status(400);
+        _res.send({
+          statusCode: 400,
+          success: false,
+          statusMessage: err.message,
+        });
+      } else {
+        const { email, password, _id } = res;
+        let data = {
+          email,
+          password,
+          id: _id,
+        };
+        _res.status(200);
+        _res.send({ statusCode: 200, success: true, data });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    _res.status(401);
+    _res.send({
+      statusCode: 401,
+      success: false,
+      statusMessage: error.message,
+    });
+  }
 };
