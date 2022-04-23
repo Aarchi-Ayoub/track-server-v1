10dee86d8ead0aa905112f5656cbda2645339706
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-04-23 21:35:54 +0000

    save pwd hashed

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 // Packages
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -13,6 +14,35 @@ const userSchema = new mongoose.Schema({
     minlength: 6,
     maxlength: 20,
   },
+});
+
+/**
+ * Additional call before instance a user in our db
+ */
+userSchema.pre("save", function (next) {
+  // Get ref of the user
+  const user = this;
+  // The user hasn't modified his pwd
+  if (!user.isModified("password")) {
+    // Give up
+    return next();
+  }
+  // Generate the salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    // Hash the salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      // Update the user pwd
+      user.password = hash;
+      // Complete the login
+      next();
+    });
+  });
 });
 
 mongoose.model("User", userSchema);
