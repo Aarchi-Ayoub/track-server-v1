c4705faa41b5ae41f6014baebf2f5ab8de4ee213
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-04-23 18:32:20 +0000

    check the new middleware

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,11 +6,16 @@ const { singUp } = require("../controllers/Auth");
 
 // Middlewares
 const { userSignUpValidator } = require("../middlewares/AuthValidate");
+const { checkAuth } = require("../middlewares/CheckAuth");
 
 // Instance the router
 const router = express.Router();
 
 // Route
 router.post("/auth/singUp", userSignUpValidator, singUp);
+
+router.get("/auth", checkAuth, (req, res) => {
+  res.send(`Your email adress is : ${req.user.email}`);
+});
 
 module.exports = router;
