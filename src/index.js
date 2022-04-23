fc00e3998a3966bd76d19320e458df1c3adf8923
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-04-23 12:39:48 +0000

    use declared variables and start connection with server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
+// Packages
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// App config
 const app = express();
 
-app.get('/', (_req ,_res)=>{
-    _res.send('Hello there');
-})
+// Use the env variables
+const port = process.env.PORT || 3000;
+const serverHost = process.env.SERVER || "";
+const userName = process.env.USERNAME || "";
+const password = process.env.PASSWORD || "";
+const dbQuery = process.env.DBQUERY || "";
 
-app.listen(3000, ()=>{
-    console.log('Server set on port 3000')
-})
+// DataBase path
+const db = `${serverHost}://${userName}:${password}@cluster0.kneru.mongodb.net/${dbQuery}`;
+// Home page
+app.get("/", (_req, _res) => {
+  _res.send("Hello there");
+});
+
+// Start connection with the db
+mongoose
+  .connect(db)
+  .then(() => console.log("Connect to database..."))
+  .catch((err) => console.log("Can not connect to database == ", err));
+
+// Server port
+app.listen(port, () => {
+  console.log(`Server set on port ${port}...`);
+});
