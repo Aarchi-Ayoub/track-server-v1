80d875cabf641cc939a28db55d20f32bb9f76654
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-04-23 17:16:41 +0000

    handling the singup errors

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,10 +4,13 @@ const express = require("express");
 // Controllers
 const { singUp } = require("../controllers/Auth");
 
+// Middlewares
+const { userSignUpValidator } = require("../middlewares/AuthValidate");
+
 // Instance the router
 const router = express.Router();
 
 // Route
-router.post("/auth/singUp", singUp);
+router.post("/auth/singUp", userSignUpValidator, singUp);
 
 module.exports = router;
